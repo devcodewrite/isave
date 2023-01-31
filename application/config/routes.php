0accb58d5ca4ec025cc7337f0a0e12de4acb5edf
Eric Mensah <ericmensah@codewrite.org>
2023-01-31 14:38:46 +0000

    fixed bankaccounts route

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,7 +53,7 @@ $route['default_controller'] = 'auth/index';
 $route['forgot-password'] = 'auth/forgot_password';
 $route['reset-password'] = 'auth/reset_password';
 $route['dashboard'] = 'dashboard/index';
-$route['bankaccounts'] = 'BankAccounts';
+$route['bankaccounts/(:any)'] = 'BankAccounts/$1';
 $route['(:any)/(\d+)'] = '$1/view/$2';
 $route['(:any)/(\d+)/edit'] = '$1/edit/$2';
 
