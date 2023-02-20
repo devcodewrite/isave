6a4b957074a040679ba795e30f4fc86fb1971d2f
Eric Mensah <ericmensah@codewrite.org>
2023-02-20 20:07:26 +0000

    added link to codewrite

--- application/views/templates/app_footer.php
+++ application/views/templates/app_footer.php
@@ -2,18 +2,18 @@
     <div class="app-footer">
         <div class="app-footer__inner">
             <div class="app-footer-left">
-               
+
             </div>
             <div class="app-footer-right">
                 <ul class="header-megamenu nav">
                     <li class="nav-item">
                         <a class="nav-link">
-                        Copyright © <?=$this->config->item('app_name') ?> <?=$this->config->item('app_version') ?> <?=$this->config->item('app_version_year') ?>
+                            Copyright © <?= $this->config->item('app_name') ?> <?= $this->config->item('app_version') ?> <?= $this->config->item('app_version_year') ?>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link">
-                        <a href="https://codewrite.org">Codewrite Technology Ltd</a>.
+                        <a href="https://codewrite.org" class="nav-link">
+                            Codewrite Technology Ltd.
                         </a>
                     </li>
                 </ul>
