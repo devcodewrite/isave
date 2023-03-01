82ad0439863ffecdf9c55c10ab9f845e38570e0d
Eric Mensah <ericmensah@codewrite.org>
2023-03-01 01:05:59 +0000

    association accounts

--- application/controllers/Associations.php
+++ application/controllers/Associations.php
@@ -52,7 +52,7 @@ class Associations extends MY_Controller
 
         if ($tran) {
             $statement->id = $tran->tdate;
-            $statement->total_amount = $tran->momo_deposits;
+            $statement->total_amount = $tran->cash_deposits;
         }
         $data = [
             'association_id' => $association_id,
