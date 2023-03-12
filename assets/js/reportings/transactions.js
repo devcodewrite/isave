b86fcbb3eaf84088e2a9be4b1aeed7de8d7c6722
Eric Mensah <ericmensah@codewrite.org>
2023-03-12 09:18:25 +0000

    transaction added js

---
+++ assets/js/reportings/transactions.js
@@ -0,0 +1,38 @@
+let transactionTable;
+
+var minDate, maxDate;
+
+// Create date inputs
+minDate = $("#transaction-date-from");
+
+maxDate = $("#transaction-date-to");
+
+$.fn.DataTable.ext.search.push(function (settings, data, dataIndex) {
+  var min = new Date(minDate.val());
+  var max = new Date(maxDate.val());
+  var date = new Date(data[0]);
+
+  if (
+    (min <= date && max >= date) ||
+    (minDate.val() === "" && maxDate.val() === "")
+  ) {
+    return true;
+  }
+  return false;
+});
+
+transactionTable = $("#dt-transactions").DataTable({
+  responsive: !0,
+  dom: "lBftip",
+  buttons: ["print", "pdf", "excel"],
+  order: [[0, "desc"]],
+});
+
+$(".transaction-filter-clear").on("click", function (params) {
+  $("#transaction-date-from,#transaction-date-to").val("");
+  transactionTable.draw();
+});
+
+$(".transaction-filter").on("click", function (params) {
+  transactionTable.draw();
+});
