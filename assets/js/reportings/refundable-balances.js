4279c20e61db09f7fed7a98ac6077161e893919d
Eric Mensah <ericmensah@codewrite.org>
2023-03-13 14:13:06 +0000

    refundable balances done

--- assets/js/reportings/refundable-balances.js
+++ assets/js/reportings/refundable-balances.js
@@ -1,27 +1,6 @@
 let table;
 
 $(function () {
-  var minDate, maxDate;
-
-  // Create date inputs
-  minDate = $("#transaction-date-from");
-  maxDate = $("#transaction-date-to");
-
-  $.fn.DataTable.ext.search.push(function (settings, data, dataIndex) {
-    var min = new Date(minDate.val());
-    var max = new Date(maxDate.val());
-    var date = new Date(data[1]);
-
-    if (
-      
-      (min <= date && max >= date) ||
-      (minDate.val() === "" && maxDate.val() === "")
-    ) {
-      return true;
-    }
-    return false;
-  });
-
   // transactions
   table = $("#dt-transactions").DataTable({
     responsive: !0,
